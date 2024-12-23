4d0fa14d079649d54ad4a96220f3daef359a747f
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-23 17:08:07 +0500

    category and single blog page

---
+++ src/app/blogs/[category]/components/banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <main className='bg-[#252C39] py-24 text-center text-6xl font-bold text-gray-200'>
+        AI Videos
+    </main>
+  )
+}
+
+export default Banner
--- src/app/blogs/components/footer.jsx
+++ src/app/blogs/components/footer.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-        <footer className='border-t border-b py-8'>
-            <div className='flex flex-col md:flex-row md:justify-between md:items-center container mx-auto px-3'>
-                <ul className='flex flex-col md:flex-row gap-5 text-white lg:text-xl font-semibold uppercase'>
-                    <li>Animation</li>
-                    <li>AI Content Creation</li>
-                    <li>AI Commercials</li>
-                    <li>AI Videos</li>
-                </ul>
-                <ul  className='flex mt-4 md:mt-0 flex-col md:flex-row gap-5 text-white lg:text-xl font-semibold uppercase'>
-                <li>Facebook</li>
-                    <li>Instagram</li>
-                    <li>Twitter X</li>
-                </ul>
-            </div>
-        </footer>
-            <p className='text-center text-white py-8 font-semibold md:text-2xl'>© 2024 LeyLine</p>
+            <footer className='border-t border-b py-8'>
+                <div className='flex flex-col md:flex-row md:justify-between md:items-center max-w-[1200px] mx-auto px-3'>
+                    <ul className='flex flex-col md:flex-row gap-5 text-white font-semibold uppercase'>
+                        <li>Animation</li>
+                        <li>AI Content Creation</li>
+                        <li>AI Commercials</li>
+                        <li>AI Videos</li>
+                    </ul>
+                    <ul className='flex mt-4 md:mt-0 flex-col md:flex-row gap-5 text-white font-semibold uppercase'>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter X</li>
+                    </ul>
+                </div>
+            </footer>
+            <p className='text-center text-white py-8 font-semibold md:text-lg'>© 2024 LeyLine</p>
         </>
     )
 }
